4d2f357ee391afc932a53a84c7fb934d56da17fc
alexbotha <alexbotha@hotmail.co.uk>
2022-07-01 15:52:36 +0100

    commit tree 5d0fc3cc3076a0d927e4096d048cfa7413d6e9a5

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance) {
+const headQ = 42
+if (distance >= 42){
+return (distance - headQ)
+}
+else if (distance < 42){
+  return Math.abs(distance - headQ)
+}
+}
+//////////////////////////////////////////////////////////////////////////////
+
+function distanceFromHqInFeet(blockNumber) {
+ return distanceFromHqInBlocks(blockNumber) * 264;
+ }
+
+ ///////////////////////////////////////////////////////////////////////
+
+function distanceTravelledInFeet(start, destination){
+  return Math.abs(start - destination) * 264
+    }
+
+//////////////////////////////////////////////////////////
+
+function calculatesFarePrice(start, destination){
+  if (distanceTravelledInFeet(start, destination) < 400){
+    return (0)
+
+  } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+    return (distanceTravelledInFeet(start, destination) - 400) * 0.02
+
+  } else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+    return (25)
+
+} else (distanceTravelledInFeet(start, destination) > 2500)
+  return('cannot travel that far')
+}
+
+
+
+
+
